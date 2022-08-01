e4b7d9e32bcfebde257ef478c76a613299c9c035
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-01 16:22:46 +0300

    Updated the default routes of signed up users to be the groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root to: 'groups#index', as: :authenticated_root
+  end
   resources :groups
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
