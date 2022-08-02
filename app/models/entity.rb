c4e8923fbe09e89fe4a1c346445ecd018802271c
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-02 07:50:42 +0300

    Update created the associations btwn user, groups amd entities

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,2 +1,4 @@
 class Entity < ApplicationRecord
+  belongs_to :user 
+  belongs_to :Group
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,5 @@
 class Group < ApplicationRecord
   has_one_attached :icon
+  belongs_to :user
+  has_many :entities
 end
