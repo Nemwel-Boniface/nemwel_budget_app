d95ba375e5b1a9926bc2b981a7fda16984c0f47e
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-08-03 07:57:46 +0300

    Update wrote unit tests for the Users model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before(:each) { @person = User.new(password: 'nemwel123', email: 'nemwel@gmail.com') }
+  it 'name should be present' do
+    expect(@person).to_not be_valid
+
+    @person.name = "Nemwel"
+    expect(@person).to be_valid
+  end
+
+  it 'password should be present' do
+    expect(@person).to_not be_valid
+    @person.password = nil
+    expect(@person).to_not be_valid
+  end
+
+  it 'email should be present' do
+    expect(@person).to_not be_valid
+    @person.email = nil
+    expect(@person).to_not be_valid
+  end
+end
